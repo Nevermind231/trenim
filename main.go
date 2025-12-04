ef424096fb1d158ba3e310077f2d4959f40ea2f4
nevermind <kolya.kulebaa@gmail.com>
2025-12-04 07:28:44 +0200

    upd

--- main.go
+++ main.go
@@ -3,36 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
+type Records struct {
+	Name        string
+	Try         int
+	UserNumbers []int
+	Success     int
+}
 type User struct {
-	Name         string
-	userNumber   int
-	Try          int
+	UserNumber   int
 	Limit        int
 	randomNumber int
+	Try          int
 }
 
 func main() {
 
+	var records Records
 	var name string
 	fmt.Println("Введите ваше Имя:")
 	fmt.Scanln(&name)
-
+	records.Name = name
 	user := User{
-		Name: name,
+
+		Limit: 5,
+		Try:   0,
 	}
-	user.startGame()
-	fmt.Println("Твоя профиль:", user.Name)
-	fmt.Println("Количество потраченных попыток:", user.Try)
+
+	records.UserNumbers = []int{}
+
+	user.startGame(&records)
+
+	saveToFile(records, user)
 }
 func (u *User) genRandomNumber() {
 	rand.Seed(time.Now().UnixNano())
 	u.randomNumber = rand.Intn(10) + 1
 }
 
-func (u *User) startGame() {
+func (u *User) startGame(records *Records) {
 	u.Limit = 5
 	u.Try = 0
 	u.genRandomNumber()
@@ -40,18 +52,21 @@ func (u *User) startGame() {
 	fmt.Println("Угадай число от 1 до 10")
 	for u.Try < u.Limit {
 		fmt.Println("Введите ваше число")
-		fmt.Scanln(&u.userNumber)
-		if u.userNumber < 1 || u.userNumber > 10 {
+		fmt.Scanln(&u.UserNumber)
+		if u.UserNumber < 1 || u.UserNumber > 10 {
 			fmt.Println("Ошибка ввода. Число должно быть от 1 до 10")
 			continue
 		}
 		u.Try++
-		if u.userNumber < u.randomNumber {
+		records.UserNumbers = append(records.UserNumbers, u.UserNumber)
+
+		if u.UserNumber < u.randomNumber {
 			fmt.Println("Бери повыше")
-		} else if u.userNumber > u.randomNumber {
+		} else if u.UserNumber > u.randomNumber {
 			fmt.Println("Маленько меньше")
 		} else {
 			fmt.Println("Красавчик ты угадал")
+			records.Success = u.Try
 			return
 		}
 		if u.Try < u.Limit {
@@ -61,4 +76,39 @@ func (u *User) startGame() {
 
 		}
 	}
+}
+func saveToFile(records Records, u User) {
+	file, err := os.OpenFile("records.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Ошибка при открытии файла:", err)
+		return
+	}
+	defer file.Close()
+	_, err = fmt.Fprintf(file, "Имя: %s Числа: ", records.Name)
+	if err != nil {
+		fmt.Println("Ошибка при записи имени и чисел в файл:", err)
+		return
+	}
+	for _, num := range records.UserNumbers {
+		_, err := fmt.Fprintf(file, "%d ", num)
+		if err != nil {
+			fmt.Println("Ошибка при записи числа в файл:", err)
+			return
+		}
+	}
+	if records.Success > 0 {
+		_, err = fmt.Fprintf(file, "Угадал на попытке: %d Загаданное число: %d\n", records.Success, u.randomNumber)
+		if err != nil {
+			fmt.Println("Ошибка при записи в файл:", err)
+			return
+		}
+	} else {
+		_, err = fmt.Fprintf(file, "Не угадал число за %d попыток. Загаданное число: %d\n", len(records.UserNumbers), u.randomNumber)
+		if err != nil {
+			fmt.Println("Ошибка при записи в файл:", err)
+			return
+		}
+	}
+	fmt.Println("Ваш рекорд сохранён.")
+
 }
